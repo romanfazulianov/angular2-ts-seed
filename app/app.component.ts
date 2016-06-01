2c2c2b8e007f3f646d3c14c01c9e23de5e361452
romanfazulianov <kurt10frg@gmail.com>
2016-06-01 23:18:16 +0400

    init repo

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
+/**
+ * Created by faz on 01.06.2016.
+ */
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
+/**
+ * Created by faz on 01.06.2016.
+ */
