670809e41992cb1831b2f7730e1692031d4bd8ad
romanfazulianov <kurt10frg@gmail.com>
2016-06-06 06:24:25 +0400

    fix project name to use with node 6.2.0

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: '<h1>Angular2 TypeScript seed written on 03-05 June 2016</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
